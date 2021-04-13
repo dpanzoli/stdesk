213c9d801dabbc3acae09ce8bdf74efe18354599
= <=>
2021-04-13 22:14:25 +0200

    tentative mysql

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express')
+const mysql = require('mysql');
 var app = express()
 
+const db = mysql.createConnection({
+	host: "localhost",
+	user: "nom_utilisateur",
+	password: "mot_de_passe_utilisateur"
+});
+
 app.get('/', function(req, res) {
-  res.send(process.env.SCALINGO_MYSQL_URL+", "+process.env.DATABASE_URL) 
+  res.write(process.env.SCALINGO_MYSQL_URL)
+  res.write(process.env.DATABASE_USER)
+  res.write(process.env.DATABASE_PWD)
+  res.end()
 })
 
 var server = app.listen(process.env.PORT || 3000, function() {
