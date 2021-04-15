5e2aca7c223fc70391fc981f2d73b49d60ecfa58
= <=>
2021-04-15 22:24:08 +0200

    Envoi des mails

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
-var express = require('express')
+var express = require('express');
 const mysql = require('mysql');
-var app = express()
+var app = express();
 
 const db = mysql.createConnection({
 	host: process.env.DATABASE_HOST,
@@ -11,6 +11,19 @@ const db = mysql.createConnection({
 	password: process.env.DATABASE_PWD,
 	database: process.env.DATABASE_DBNAME
 });
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+	host: process.env.EMAIL_HOST,
+	port: 465,
+	secure: true, // upgrade later with STARTTLS
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PWD
+	}
+});
+
 
 app.get('/', function(req, res) {
 
@@ -24,9 +37,39 @@ app.get('/', function(req, res) {
 	});
 });
 
+app.get('/notify', function(req, res) {
+
+	transporter.sendMail({
+		from: 'Département ST <david.panzoli@univ-jfc.fr>',
+		to: 'david.panzoli@univ-jfc.fr',
+		subject: 'Message du département ST',
+		text: 'Corps du message'
+	}, function(error, info){
+		if (error) {
+			console.log(error);
+			res.sendStatus(500);
+		} else {
+			res.sendStatus(200);
+		}
+	}); 
+});
+
 var server = app.listen(process.env.PORT || 3000, function() {
-  var host = server.address().address
-  var port = server.address().port
-  console.log(`App listening at http://${host}:${port}`)
-})
+	var host = server.address().address
+	var port = server.address().port
+	console.log(`App listening at http://${host}:${port}`)
+/*
+	transporter.verify(function(error, success) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log("Server is ready to take our messages");
+		}
+	});
+*/
+	
+
+});
+
+
 
